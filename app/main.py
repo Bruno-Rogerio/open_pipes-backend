cf10b7a6c488416ca266f7e040ea4990473b5bfa
Bruno R. Oliveira <168135189+Bruno-Rogerio@users.noreply.github.com>
2024-12-28 18:25:25 -0300

    Atualizado configuração de CORS para incluir URL do Render

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ try:
     # Configuração CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000", "https://seu-frontend-no-netlify.netlify.app"],
+        allow_origins=["http://localhost:3000", "https://open-pipes-backend.onrender.com"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
